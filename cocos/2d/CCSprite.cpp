7d91ae15f63472a280d15f0b14cd90685227667d
Julien Jorge <julien.jorge@stuff-o-matic.com>
2017-01-12 10:29:26 +0100

    Restore the vertices color when updating the polygon of a sprite.

--- cocos/2d/CCSprite.cpp
+++ cocos/2d/CCSprite.cpp
@@ -667,6 +667,8 @@ void Sprite::updatePoly()
         // probably we can update the _trianglesCommand directly
         // to avoid memcpy'ing stuff
         _polyInfo.setTriangles(triangles);
+
+        updateColor();
     }
 }
 
