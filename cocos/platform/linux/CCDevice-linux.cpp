dc7d2b982f047679be3762194773d50cbc5e1e26
Julien Jorge <julien.jorge@stuff-o-matic.com>
2017-01-12 11:50:03 +0100

    Fix incorrect multi-line display of Linux labels.
    The beginning of the break index range was not updated when a new range was
    found.

--- cocos/platform/linux/CCDevice-linux.cpp
+++ cocos/platform/linux/CCDevice-linux.cpp
@@ -54,7 +54,7 @@ struct LineBreakGlyph {
     FT_UInt glyphIndex;
     int paintPosition;
     int glyphWidth;
-
+    
     int bearingX;
     int kerning;
     int horizAdvance;
@@ -235,12 +235,13 @@ public:
             }
 
             if (iswspace(unicode)) {
-                prevGlyphIndex = glyphIndex;
-                prevCharacter = unicode;
                 lastBreakIndex = currentLine.glyphs.size();
 
-                if (firstBreakIndex == -1)
+                if (!iswspace(prevCharacter))
                     firstBreakIndex = lastBreakIndex;
+
+                prevGlyphIndex = glyphIndex;
+                prevCharacter = unicode;
             } else {
                 if (iswspace(prevCharacter))
                     lastBreakIndex = currentLine.glyphs.size();
