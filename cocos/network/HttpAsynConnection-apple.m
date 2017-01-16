2ad0123720fe1a5a937f03abc5638e7cf05a7c5a
Julien Jorge <julien.jorge@stuff-o-matic.com>
2017-01-16 11:36:53 +0100

    Use synchronous http request on Apple devices.
    Since HttpClient already does the requests in threads, there's no need to
    create another thread for the request itself.

--- cocos/network/HttpAsynConnection-apple.m
+++ cocos/network/HttpAsynConnection-apple.m
@@ -45,7 +45,6 @@
 @synthesize responseError = responseError;
 @synthesize connError = connError;
 @synthesize conn = conn;
-@synthesize finish = finish;
 @synthesize runLoop = runLoop;
 
 - (void)dealloc
@@ -58,7 +57,7 @@
     [conn release];
     [runLoop release];
     [connError release];
-    
+
     [super dealloc];
 }
 
@@ -68,44 +67,31 @@
     NSLog(@"Starting to load %@", srcURL);
 #endif
     
-    finish = false;
-
-    self.responseData = [NSMutableData data];
     getDataTime = 0;
 
     self.responseError = nil;
-    self.connError = nil;
-    
-    // create the connection with the target request and this class as the delegate
-    self.conn = [[[NSURLConnection alloc] initWithRequest:request
-                                                 delegate:self
-                                         startImmediately:NO] autorelease];
-    
-    [self.conn scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    
-    // start the connection
-    [self.conn start];
-}
 
-#pragma mark NSURLConnectionDelegate methods
-/**
- * This delegate method is called when the NSURLConnection connects to the server.  It contains the 
- * NSURLResponse object with the headers returned by the server.  This method may be called multiple times.
- * Therefore, it is important to reset the data on each call.  Do not assume that it is the first call
- * of this method.
- **/
-- (void) connection:(NSURLConnection *)connection 
- didReceiveResponse:(NSURLResponse *)response {
+    NSHTTPURLResponse* response = nil;
+    NSError* error = nil;
+    self.responseData =
+        [ NSURLConnection sendSynchronousRequest:request
+                               returningResponse:&response
+                                           error:&error ];
+    self.responseHeader = [response allHeaderFields];
+    responseCode = response.statusCode;
+    self.statusString =
+        [ NSHTTPURLResponse localizedStringForStatusCode:responseCode ];
+
+    if( error != nil )
+    {
+        self.connError = error;
+        return;
+    }
+
 #if defined( COCOS2D_DEBUG ) && ( COCOS2D_LOG_LEVEL != 0 )
     NSLog(@"Received response from request to url %@", srcURL);
 #endif
     
-    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-    //NSLog(@"All headers = %@", [httpResponse allHeaderFields]);
-    self.responseHeader = [httpResponse allHeaderFields];
-
-    responseCode = httpResponse.statusCode;
-    self.statusString = [NSHTTPURLResponse localizedStringForStatusCode:responseCode];
     if(responseCode == 200)
         self.statusString = @"OK";
  
@@ -120,108 +106,12 @@
     */
     if (responseCode < 200 || responseCode >= 300)
     {// something went wrong, abort the whole thing
-        self.responseError = [NSError errorWithDomain:@"CCBackendDomain"
-                                            code:responseCode
-                                        userInfo:@{NSLocalizedDescriptionKey: @"Bad HTTP Response Code"}];        
-    }
-    
-    [responseData setLength:0];
-}
-
-/**
- * This delegate method is called for each chunk of data received from the server.  The chunk size
- * is dependent on the network type and the server configuration.  
- */
-- (void)connection:(NSURLConnection *)connection 
-    didReceiveData:(NSData *)data
-{
-    //NSLog(@"get some data");
-    [responseData appendData:data];
-    getDataTime++;
-}
-
-/**
- * This delegate method is called if the connection cannot be established to the server.  
- * The error object will have a description of the error
- **/
-- (void)connection:(NSURLConnection *)connection 
-  didFailWithError:(NSError *)error
-{
-    //NSLog(@"Load failed with error %@", [error localizedDescription]);
-    self.connError = error;
-    
-    finish = true;
-}
-
-/**
- * This delegate method is called when the data load is complete.  The delegate will be released 
- * following this call
- **/
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection
-{
-    finish = true;
-}
-
-//Server evaluates client's certificate
-- (BOOL) shouldTrustProtectionSpace:(NSURLProtectionSpace*)protectionSpace
-{
-    if(sslFile == nil)
-        return YES;
-    //load the bundle client certificate
-    NSString *certPath = [[NSBundle mainBundle] pathForResource:sslFile ofType:@"der"];
-    NSData *certData = [[NSData alloc] initWithContentsOfFile:certPath];
-    CFDataRef certDataRef = (CFDataRef)certData;
-    SecCertificateRef cert = SecCertificateCreateWithData(NULL, certDataRef);
-    
-    //Establish a chain of trust anchored on our bundled certificate
-    CFArrayRef certArrayRef = CFArrayCreate(NULL, (void*)&cert, 1, NULL);
-    SecTrustRef serverTrust = protectionSpace.serverTrust;
-    SecTrustSetAnchorCertificates(serverTrust, certArrayRef);
-    
-    //Verify that trust
-    SecTrustResultType trustResult;
-    SecTrustEvaluate(serverTrust, &trustResult);
-    
-    if(trustResult == kSecTrustResultRecoverableTrustFailure)
-    {
-        CFDataRef errDataRef = SecTrustCopyExceptions(serverTrust);
-        SecTrustSetExceptions(serverTrust, errDataRef);
-        SecTrustEvaluate(serverTrust, &trustResult);
-        CFRelease(errDataRef);
-    }
-    [certData release];
-    if (cert)
-    {
-        CFRelease(cert);
-    }
-    if (certArrayRef) 
-    {
-        CFRelease(certArrayRef);
-    }
-    //Did our custom trust chain evaluate successfully?
-    return trustResult == kSecTrustResultUnspecified || trustResult == kSecTrustResultProceed;    
-}
-
-- (void) connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
-{
-    id <NSURLAuthenticationChallengeSender> sender = challenge.sender;
-    NSURLProtectionSpace *protectionSpace = challenge.protectionSpace;
-    
-    //Should server trust client?
-    if([self shouldTrustProtectionSpace:protectionSpace])
-    {
-        SecTrustRef trust = [protectionSpace serverTrust];
-//        
-//        SecCertificateRef certificate = SecTrustGetCertificateAtIndex(trust, 0);
-//        
-//        NSData *serverCertificateData = (NSData*)SecCertificateCopyData(certificate);
-//        NSString *serverCertificateDataHash = [[serverCertificateData base64EncodedString] ]
-        NSURLCredential *credential = [NSURLCredential credentialForTrust:trust];
-        [sender useCredential:credential forAuthenticationChallenge:challenge];
-    }
-    else
-    {
-        [sender cancelAuthenticationChallenge:challenge];
+        self.responseError =
+            [ NSError errorWithDomain:@"CCBackendDomain"
+                      code:responseCode
+                      userInfo:
+                          @{ NSLocalizedDescriptionKey:
+                             @"Bad HTTP Response Code" } ]; 
     }
 }
 
