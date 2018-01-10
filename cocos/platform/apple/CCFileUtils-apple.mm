bf97b83d75fefc8583047048d54e3cb19a80ab50
Julien Jorge <julien.jorge@stuff-o-matic.com>
2018-01-10 13:24:33 +0100

    Fix compilation issues with XCode 9.

--- cocos/platform/apple/CCFileUtils-apple.mm
+++ cocos/platform/apple/CCFileUtils-apple.mm
@@ -286,7 +286,7 @@ bool FileUtilsApple::isFileExistInternal(const std::string& filePath) const
     return ret;
 }
 
-static int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+static int unlink_cb_apple(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
 {
     auto ret = remove(fpath);
     if (ret)
@@ -305,7 +305,7 @@ bool FileUtilsApple::removeDirectory(const std::string& path)
         return false;
     }
 
-    if (nftw(path.c_str(),unlink_cb, 64, FTW_DEPTH | FTW_PHYS))
+    if (nftw(path.c_str(),unlink_cb_apple, 64, FTW_DEPTH | FTW_PHYS))
         return false;
     else
         return true;
