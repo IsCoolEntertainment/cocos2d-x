321d0f72269c270150e4e1d530b87b38418ecc07
Julien Jorge <julien.jorge@stuff-o-matic.com>
2018-07-11 10:20:46 +0200

    Add an audio mode to keep the music playing with other apps.

--- cocos/audio/ios/CDAudioManager.m
+++ cocos/audio/ios/CDAudioManager.m
@@ -363,6 +363,12 @@ static BOOL configured = FALSE;
             willPlayBackgroundMusic = YES;
             break;
             
+        case kAMM_FxPlusMixedMusic:
+            CDLOGINFO(@"Denshion::CDAudioManager -  Effects and music will be shared");
+            _audioSessionCategory = AVAudioSessionCategoryAmbient;
+            willPlayBackgroundMusic = YES;
+            break;
+            
         case kAMM_MediaPlayback:
             //Use audio exclusively, ignore mute switch and sleep
             CDLOGINFO(@"Denshion::CDAudioManager -  Media playback mode, audio will be exclusive");
